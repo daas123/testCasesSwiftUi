04cedb78ad66b400d21432d080ad34cd2655afa2
saad <saadneosoft@gmail.com>
2023-11-28 11:10:12 +0530

    Initial Commit

---
+++ testCasesSwiftUi/testCasesSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCasesSwiftUiApp.swift
+//  testCasesSwiftUi
+//
+//  Created by Neosoft on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct testCasesSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
